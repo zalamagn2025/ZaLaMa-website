f6fcfe1e95a1356ac5dddef81bbe499bbd37b393
KarfallaDiaby <diabykarfalla2@gmail.com>
2025-04-29 15:24:29 +0000

    Ajout de la section Feature pour le home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import { Header } from "@/components/layout/Header";
 import { Hero } from "@/components/sections/Hero";
+import { HomeFeature } from "@/components/sections/HomeFeature";
 
 export default function Home() {
   return (
     <div>
       <Header/>
       <Hero/>
+      <HomeFeature/>
     </div>
     
   );
