88f0fc77fd4a7d68b769ae6184c97ff8ffadd900
KarfallaDiaby <diabykarfalla2@gmail.com>
2025-04-30 10:06:12 +0000

    ajustation

--- src/components/sections/HomeFeature.tsx
+++ src/components/sections/HomeFeature.tsx
@@ -4,18 +4,18 @@ import React from 'react'
 
 export const HomeFeature = () => {
   return (
-<section className="bg-white dark:bg-gray-900">
+<section className="dark:bg-gray-900">
     <div className="container px-6 py-10 mx-auto lg:px-20">
-        <h1 className="text-3xl tracking-tighter font-black text-gray-800 capitalize lg:text-4xl dark:text-white">
-            La qualité Zalama, <br/> une promesse tenue !
-        </h1>
-
-        <div className="mt-2">
-            <span className="inline-block w-40 h-1 bg-[#10059F] rounded-full"></span>
-            <span className="inline-block w-3 h-1 ml-1 bg-[#10059F] rounded-full"></span>
-            <span className="inline-block w-1 h-1 ml-1 bg-[#10059F] rounded-full"></span>
+        <div className='section-heading flex flex-col items-center'>
+            <h2 className="section-title">
+                La qualité Zalama, une promesse tenue
+            </h2>
+            <div className="mt-2">
+                <span className="inline-block w-40 h-1 bg-[#10059F] rounded-full"></span>
+                <span className="inline-block w-3 h-1 ml-1 bg-[#10059F] rounded-full"></span>
+                <span className="inline-block w-1 h-1 ml-1 bg-[#10059F] rounded-full"></span>
+            </div>
         </div>
-
         <div className="mt-8 xl:mt-12 lg:flex lg:items-center">
             <div className="grid w-full grid-cols-1 gap-8 lg:w-1/2 xl:gap-16 md:grid-cols-2">
                 
