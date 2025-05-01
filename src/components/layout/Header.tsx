1cb139752cad2334685be6702fb45634e81c334b
KarfallaDiaby <diabykarfalla2@gmail.com>
2025-05-01 11:42:16 +0000

    ajout du footer

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -7,7 +7,7 @@ export const Header = () => {
   return (
     <header className='py-5 px-5 sticky top-0 backdrop-blur-sm z-20'>
             <div className='flex items-center justify-between'>
-                <Image src={"/images/Logo_vertical.svg"} width={130} height={0} alt={'Logo de ZaLaMa'}/>
+                <Image src={"/images/zalama-logo.svg"} width={130} height={0} alt={'Logo de ZaLaMa'}/>
                 <MenuIcon className='md:hidden' size={32}/>
                 <nav className='hidden md:flex gap-6 text-black/60 items-center'>
                     <a href="">Acceuil</a>
