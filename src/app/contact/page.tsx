8055916ed1490ddf041ccdad1a645645ae47c1f5
KarfallaDiaby <diabykarfalla2@gmail.com>
2025-05-04 17:39:18 +0000

    contact page

---
+++ src/app/contact/page.tsx
@@ -0,0 +1,15 @@
+import { Footer } from '@/components/layout/Footer'
+import { Header } from '@/components/layout/Header'
+import { Contact } from '@/components/sections/Contact/Contact'
+import React from 'react'
+
+export default function 
+() {
+  return (
+    <div>
+        <Header/>
+        <Contact/>
+        <Footer/>     
+    </div>
+  )
+}
